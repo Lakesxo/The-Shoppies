020f4278400fc0c12f8d81539221007a87c7c103
unknown <lakes.ajanaku@gmail.com>
2021-08-11 20:09:13 +0100

    updates

--- src/components/Flex.js
+++ src/components/Flex.js
@@ -5,9 +5,25 @@ import Results from './Results'
 const Flex = ({ sendItems, listArr, nomFunc, removeNomination }) => {
     const nonStyle = {display: "none"}
     const blkStyle = {display: "flex"}
+    const mediaQuery = window.innerWidth <= 500
+    const mbStyle = {display: "block"}
+    let disp = {}
+
+    if (mediaQuery && sendItems.length >= 1) {
+        disp = mbStyle
+    } else if (sendItems.length >= 1) {
+        disp = blkStyle
+    } else if (mediaQuery && listArr.length >= 1) {
+        disp = mbStyle
+    } else if (listArr.length >= 1) {
+        disp = blkStyle
+    } else {
+        disp = nonStyle
+    }
+    
 
     return (
-        <div style={sendItems.length >= 1 ? blkStyle : nonStyle} className="flex">
+        <div style={disp} className="flex">
             <Results items={sendItems} nomFunc={nomFunc} listArr={listArr} />
             <Nominations list={listArr} removeNomination={removeNomination}/>
         </div>
